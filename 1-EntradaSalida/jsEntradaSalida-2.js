4f829bda06f85787854fa100908a26928821ce6b
Agustin Esquivel <40763658+agustinesquivel@users.noreply.github.com>
2018-07-02 12:05:58 -0300

    Ejercicios 1 2 3

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,15 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	//var person = prompt("Please enter your name", "Harry Potter");
+	/* comentario 
+	en 
+	bloque*/
+	var nombre;
+	nombre=prompt("ingrese su nobre", "natalia natalia");
+	alert ("su nombre es: "+nombre);
+	/*promp y alert son ventanas emergentes. promp pide datos
+	y alert es solo alerta*/
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,16 @@
 /*Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	/*alert ("ejercicio 3");
+	console.log ("esto es un mensaje con log");
+	*/
+	var nombre
+	nombre=document.getElementById('elNombre').value ;
+	/*para sumar se indica .value al final de la linea
+	para obtener document.getElementById sin errores se presiona 
+	el comienzo hasta (.) luego get y tab shift +i*/
+	alert ("su nombre es: "+ nombre);
 }
 
 
