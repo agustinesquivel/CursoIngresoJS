a94cd1a969f8bff70466004bf844e2097a28ed2b
Agustin Esquivel <40763658+agustinesquivel@users.noreply.github.com>
2018-07-23 12:22:04 -0300

    Terminados 1 al 6

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,24 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch (mesDelAño){
+    case ("Julio"):
+    case ("Agosto"):
+        alert("Abrigate que hace frio")
+        break;
+    case ("Marzo"):
+    case ("Abril"):
+    case ("Mayo"):             
+    case ("Junio"):
+        alert ("Falta poco Para el invierno")
+        break;
+    default:
+        alert ("Ya paso el frio, ahora el calor")
+        break;
+
+
+
+}
 
 
 
