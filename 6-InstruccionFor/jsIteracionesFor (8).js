69b61eec228390d4d7d7dc87a9363607fbfb0d72
Agustin Esquivel <40763658+agustinesquivel@users.noreply.github.com>
2018-08-06 11:53:20 -0300

    ejercicio 8 terminado

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,5 +1,32 @@
 function mostrar()
 {
+var numeroingresado;
+var numerosanteriores;
+var esprimo;
+var numerorecorrido;
+
+numeroingresado=prompt("ingrese numero");
+numeroingresado=parseInt(numeroingresado);
+for(numerorecorrido=numeroingresado;numerorecorrido>1;numerorecorrido--){
+
+
+
+esprimo=0;
+for (numerosanteriores=2;numerosanteriores< numerorecorrido;numerosanteriores--){
+
+if(numerorecorrido%numerosanteriores==0){
+console.log(numerosanteriores);
+    break;
+
+}
+}
+}
+if(esprimo==0){
+    console.log(numerorecorrido+" es un numero primo");
+}/*else{
+    console.log(numeroingresado+" no es un numero primo");
+}*/
+
 
 
 
